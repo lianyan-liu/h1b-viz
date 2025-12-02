6521028b08f62d0bf08a9ca5b13a0bd7060b3635
Lianyan Liu <ll2226@cornell.edu>
2025-12-02 13:52:19 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,20 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const base = mode === "production" ? "/h1b-viz/" : "/";
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    base,
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
